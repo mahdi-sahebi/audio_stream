d24994451b181cd6115db1d693d4182abcd6839e
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-11-08 04:49:20 +0000

    [TEST]: Test cases are decalred

---
+++ test/test.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+
+
+TEST(creation, invalid)
+{
+    FAIL();
+}
+
+TEST(creation, valid)
+{
+    FAIL();
+}
+
+TEST(status, connection)
+{
+    FAIL();
+}
+
+TEST(send_buffer, small)
+{
+    FAIL();
+}
+
+TEST(send_buffer, large)
+{
+    FAIL();
+}
+
+TEST(send_buffer, large_interrupted_communication)
+{
+    FAIL();
+}
+
+TEST(send_audio, small)
+{
+    FAIL();
+}
+
+TEST(send_audio, small_high_load)
+{
+    FAIL();
+}
+
+int main()
+{
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
+
