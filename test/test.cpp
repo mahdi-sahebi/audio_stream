fa95307341e29fb8a9025d4f06645385819367f4
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-11-09 05:20:23 +0000

    [TEST]: Test of existing server connection is defined

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,17 @@
+#include <unistd.h>
 #include <memory>
 #include <iostream>
+#include <future>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <sys/wait.h>
 #include <gtest/gtest.h>
 
 
 using namespace std;
+using namespace std::chrono;
+using namespace std::this_thread;
 
 
 TEST(creation, invalid)
@@ -47,7 +55,45 @@ TEST(connection, not_ready_server)
 
 TEST(connection, ready_server)
 {
-    FAIL();
+    // TODO(MN): Fixture class
+    // TODO(MN): Check nodejs server file existance
+
+    atomic<pid_t> serverPID{0};
+
+    future<bool> serverTask = async(launch::async, [&]() {
+        const auto pid = fork();
+        if (0 == pid) {
+            cout << "Process creation failed" << endl;
+            return false;
+        }
+
+        serverPID = pid;
+        waitpid(serverPID, nullptr, 0);
+        return true;
+    });
+
+
+    ASSERT_NO_THROW(
+        /* Let server to be executed */
+        sleep_for(milliseconds(500);
+
+        auto stream = unique_ptr<AudioStream>(1024);
+        
+        const auto isConnected = stream->connect(Endpoint("127.0.0.0", 8080));
+        EXPECT_TRUE(isConnected);
+
+        const auto isDisconnected = stream->disconnect();
+        EXPECTED_TRUE(isDisconnected);
+
+        /* Stop the server */
+        if (serverPID > 0) {
+            kill(serverPID, SIGKILL);
+            serverPID = 0;
+        }
+
+        const auto serverResult = serverTask.get();
+        ASSERT_EQ(serverResult, 0);
+    );
 }
 
 TEST(send_buffer, small)
