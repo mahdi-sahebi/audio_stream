6bd4cf08171b7e88952daa376e40f77ff99544b5
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-11-14 20:26:23 +0000

    [TEST]: Nodejs server is udpated for storing files

--- test/deps/server.js
+++ test/deps/server.js
@@ -1,18 +1,35 @@
 const http = require('http');
 const fs = require('fs');
 const WebSocket = require('ws');
+const path = require('path');
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server, perMessageDeflate: false });
-const writeStream = fs.createWriteStream('received_file.bin', { flags: 'a' });
+
+// Create file in the same directory as server.js
+const filePath = path.join(__dirname, 'received_data.bin');
+console.log(`[SERVER]: Writing to: ${filePath}`);
+
+const writeStream = fs.createWriteStream(filePath, { 
+    flags: 'a',
+    flush: true
+});
+
 let totalBytes = 0;
 
 wss.on('connection', (ws) => {
+    console.log('[SERVER]: Client connected');
+    
     ws.on('message', (message, isBinary) => {
         if (isBinary) {
             totalBytes += message.length;
             writeStream.write(message);
+            console.log(`[RCV]: ${message.length}`);
         }
+    });
+    
+    ws.on('close', () => {
+        console.log(`[SERVER]: Client disconnected. Total bytes: ${totalBytes}`);
     });
 });
 
